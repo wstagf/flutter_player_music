7b0aac5382e9ff994cbf931cec5be4b164d98641
Thiago Augusto Gonçalves Fernandes Santos Silva <wstagf@gmail.com>
2020-05-04 22:18:20 -0300

    implementado request do banda model

--- lib/app/modules/player_music/player_music_controller.dart
+++ lib/app/modules/player_music/player_music_controller.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_player_music/app/models/banda_model.dart';
+import 'package:flutter_player_music/app/models/musica_model.dart';
+import 'package:flutter_player_music/app/repositories/banda_repository.dart';
 import 'package:mobx/mobx.dart';
 
 part 'player_music_controller.g.dart';
@@ -5,11 +8,19 @@ part 'player_music_controller.g.dart';
 class PlayerMusicController = _PlayerMusicBase with _$PlayerMusicController;
 
 abstract class _PlayerMusicBase with Store {
+  final BandaRepository bandaRepository;
+
+  _PlayerMusicBase(this.bandaRepository);
+
   @observable
-  int value = 0;
+  BandaModel bandaModel;
+
+  @observable
+  ObservableFuture<BandaModel> bandFuture;
 
   @action
-  void increment() {
-    value++;
+  buscarBanda(String id) async {
+    bandFuture = ObservableFuture(bandaRepository.buscarBandaPorId(id));
+    bandaModel = await bandFuture;
   }
 }
--- lib/app/modules/player_music/player_music_module.dart
+++ lib/app/modules/player_music/player_music_module.dart
@@ -1,11 +1,12 @@
 import 'package:flutter_player_music/app/modules/player_music/player_music_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_player_music/app/modules/player_music/player_music_page.dart';
+import 'package:flutter_player_music/app/repositories/banda_repository.dart';
 
 class PlayerMusicModule extends ChildModule {
   @override
   List<Bind> get binds => [
-        Bind((i) => PlayerMusicController()),
+        Bind((i) => PlayerMusicController(i.get<BandaRepository>())),
       ];
 
   @override
